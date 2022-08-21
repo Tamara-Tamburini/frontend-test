517db91f321f7c6cde1dcd20d2b8c05262c4e0d8
Tamara Tamburini <yume.no.geisha@gmail.com>
2022-08-21 23:04:11 +0200

    Details view data load

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,7 +2,7 @@ import Description from "./components/Description";
 import Actions from "./components/Actions";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {getMobileDetails } from "../../services/mobileServices"
+import { getMobileDetails } from "../../services/mobileServices";
 import { Row, Col, Image } from "antd";
 import "./Details.css";
 
@@ -10,28 +10,33 @@ function Details() {
   const { id } = useParams();
   const [mobileDetails, setMobileDetails] = useState([]);
 
-  console.log(id);
   useEffect(() => {
-    getMobileDetails(id).then((itemData) => setMobileDetails(itemData))
-  }, [])
-
-  console.log(mobileDetails);
+    getMobileDetails(id).then((itemData) => setMobileDetails(itemData));
+  }, []);
 
   return (
     <>
-      <Row className="detailsWrapper" justify="center">
-        <Col span={8} className="deviceImg">
-          <Image
-            preview={false}
-            width={200}
-            src={mobileDetails.imgUrl}
-          ></Image>
-        </Col>
-        <Col span={12} className="deviceDetails">
-          <Description />
-          <Actions />
-        </Col>
-      </Row>
+      <div className="detailsWrapper">
+        <Row className="detailsRow" justify="center">
+          <Col span={8} className="deviceImg" xs={24} sm={6} md={8} lg={8} xl={8}>
+            <Image
+              preview={false}
+              width={200}
+              src={mobileDetails.imgUrl}
+            ></Image>
+          </Col>
+          <Col span={12} className="deviceDetails" xs={24} sm={8} md={12} lg={12} xl={12}>
+            <div className="detailsTitle">
+              {mobileDetails.brand} - {mobileDetails.model}
+            </div>
+            <h2 className="detailsPrice">{mobileDetails.price}€</h2>
+            <Actions options={mobileDetails.options} />
+
+            <h2 className="descriptionTitle">Details</h2>
+            <Description details={mobileDetails} />
+          </Col>
+        </Row>
+      </div>
     </>
   );
 }
--- src/pages/details/components/Actions.jsx
+++ src/pages/details/components/Actions.jsx
@@ -1,29 +1,52 @@
-import { Row, Form, Button, Radio } from "antd";
+import { Form, Button, Radio } from "antd";
 
-function Actions() {
-    
-
-  return (
-    <Row >
-      <Form>
-        <Form.Item label="Colors">
-            <Radio.Group value="black">
-                <Radio.Button value="black">Black</Radio.Button>
-                <Radio.Button value="white">White</Radio.Button>
-            </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Storages">
-            <Radio.Group value="2000">
-                <Radio.Button value="2000">16 GB</Radio.Button>
-                <Radio.Button value="2001">32 GB</Radio.Button>
-            </Radio.Group>
-        </Form.Item>
-        <Form.Item>
-            <Button type="primary">Add to Card</Button>
-        </Form.Item>
-      </Form>
-    </Row>
-  );
+const onChange = (e) => {
+  console.log(`radio checked:${e.target.value}`);
+};
+function Actions({ options }) {
+  if (options) {
+    return (
+      <>
+        <Form>
+          {options.colors ? (
+            <Form.Item label="Colors">
+              <Radio.Group
+                onChange={onChange}
+                defaultValue={options.colors[0].code}
+              >
+                {options.colors.map((item, index) => {
+                  return (
+                    <Radio.Button key={index} value={item.code}>
+                      {item.name}
+                    </Radio.Button>
+                  );
+                })}
+              </Radio.Group>
+            </Form.Item>
+          ) : null}
+          {options.storages ? (
+            <Form.Item label="Storages">
+              <Radio.Group
+                onChange={onChange}
+                defaultValue={options.storages[0].code}
+              >
+                {options.storages.map((item, index) => {
+                  return (
+                    <Radio.Button key={index} value={item.code}>
+                      {item.name}
+                    </Radio.Button>
+                  );
+                })}
+              </Radio.Group>
+            </Form.Item>
+          ) : null}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Add to Card</Button>
+          </Form.Item>
+        </Form>
+      </>
+    );
+  }
 }
 
-export default Actions
+export default Actions;
